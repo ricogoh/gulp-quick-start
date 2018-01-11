9f1c53b32d6b2336b8948a126b516d436ed7dec9
Rico Goh <ricogoh@gmail.com>
2018-01-11 13:16:48 +0800

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp'); // npm install gulp --save-dev
 var sass = require('gulp-sass'); // npm install gulp-sass --save-dev
 var browserSync = require('browser-sync').create(); // npm install browser-sync --save-dev
 var useref = require('gulp-useref'); // npm install gulp-useref --save-dev
-// var concat = require('gulp-concat'); // npm install gulp-concat gulp-rename gulp-uglify --save-dev
 var uglify = require('gulp-uglify'); // npm install gulp-uglify --save-dev
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano') // npm install gulp-cssnano --save-dev
@@ -10,11 +9,6 @@ var imagemin = require('gulp-imagemin') // npm install gulp-imagemin --save-dev
 var cache = require('gulp-cache'); // npm install gulp-cache --save-dev
 var del = require('del'); // npm install del --save-dev
 var runSequence = require('run-sequence'); // npm install run-sequence --save-dev
-
-// runt "gulp hello"
-gulp.task('hello', function () {
-    console.log('Hello Rico');
-});
 
 // browserSync live reload server
 gulp.task('browserSync', function () {
@@ -25,15 +19,11 @@ gulp.task('browserSync', function () {
     })
 })
 
-// run "gulp sass"
 // compile css to src/css folder
 gulp.task('sass', function () {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('src/css'))
-        // .pipe(browserSync.reload({
-        //     stream: true
-        // }))
 });
 
 gulp.task('css', function () {
@@ -42,12 +32,6 @@ gulp.task('css', function () {
             stream: true
         }))
 });
-
-// // gulp.task('js', function () {
-// //     return gulp.src('src/js/**/*.js')
-// //         .pipe(concat('main.js'))
-// //         .pipe(gulp.dest('dist/js'))
-// // });
 
 gulp.task('useref', function () {
     return gulp.src('src/*.html')
@@ -74,14 +58,10 @@ gulp.task('clean:dist', function () {
     return del.sync('dist');
 })
 
-// ///////////////////// WATCH ///////////////////////
+/////////////////////////////////////////////////////////
 
-// // Gulp watch syntax
-// // run "gulp"
-//gulp.watch('src/scss/**/*.scss', ['sass']);
-
-// // Multiple gulp watch
-// // run "gulp watch"
+// run "gulp watch"
+// Multiple gulp watch
 gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('src/scss/**/*.scss', ['sass']);
     gulp.watch('src/css/**/*.css', ['css']);
@@ -91,6 +71,7 @@ gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('src/js/**/*.js', browserSync.reload);
 })
 
+// run "gulp build"
 gulp.task('build', function (callback) {
     runSequence('clean:dist',
         ['sass', 'useref', 'images', 'fonts'],
@@ -104,4 +85,3 @@ gulp.task('default', function (callback) {
         callback
     )
 })
-
